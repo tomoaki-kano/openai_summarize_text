b8d1117fd18b799fd88c64d9cebb234f19397cb1
Tomoaki KANO <tomoaki.kano@smarthr.co.jp>
2023-06-09 14:31:19 +0900

    test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import streamlit as st
+
+# テキストボックスを作成
+input_text = st.text_input('テキストを入力してください')
+
+# ボタンを作成
+button_clicked = st.button('表示')
+
+# ボタンがクリックされたらテキストを表示
+if button_clicked:
+    st.write(input_text)
