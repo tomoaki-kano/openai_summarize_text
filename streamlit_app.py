66dd326471876f9d46313754508b0e91a8b6f1c2
Tomoaki KANO <tomoaki.kano@smarthr.co.jp>
2023-06-12 17:34:53 +0900

    set model_name

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ from langchain.chains.mapreduce import MapReduceChain
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
-from langchain.chat_models import OpenAIChat
+from langchain.chat_models import ChatOpenAI
 
 
 def summarize_text(text, prompt_template, refine_template):
 
-    llm = OpenAIChat(
+    llm = ChatOpenAI(
         model_name="gpt-4",
         temperature=0
     )
