8952a1dddadee6197f6cf41a9cfc6a4e8f6f9e5f
Tomoaki KANO <tomoaki.kano@smarthr.co.jp>
2023-07-04 09:22:38 +0900

    fix summarize_text arguments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
+'''
+Streamlit: 
+'''
+
 import streamlit as st
 
 import langchain
 from langchain.llms import OpenAI
 
 
-def summarize_text(text, prompt):
+def summarize_text(text):
 
     llm = OpenAI(
         model_name="gpt-4-0613",
